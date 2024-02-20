30ccad1b488a7ffe5a3cbb33363d5edba5b3267c
sthingtoeat <qly@1040606497@qq.com>
2024-02-20 14:11:23 +0800

    新增了登录页面重定向

--- backendweb/src/router/index.js
+++ backendweb/src/router/index.js
@@ -6,6 +6,11 @@ import UserFriendView from '@/views/user/UserFriendView'
                                 //@是指在src目录下
 const routes = [
   {
+    path:"/",
+    name:"home",
+    redirect:"/login"           //重定向到登录页面
+  },
+  {
     path:"/user",              //localhost:8080/user/
     name:"user_index_view",     //只是一个名字,可用于页面跳转router.push({name:"user_index_view"})
     component:UserIndexView,    //索引到对应的页面，而页面由上方导入
